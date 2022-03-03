cf5c1f8d41349a8f2eb4fa473af697266cd4829e
Alexandru Stefanescu <alexandru@stefanescu@dekra.com>
2022-03-03 17:15:30 +0100

    changed the id form to be able to identify better what option it is.

--- src/app/mock-data/fruits.ts
+++ src/app/mock-data/fruits.ts
@@ -1,8 +1,8 @@
 import { Fruits } from "../models/fruits-model";
 
 export const FruitsData: Fruits[] = [
-  { value: '0', label: 'Apple' },
-  { value: '1', label: 'Banana' },
-  { value: '2', label: 'Orange' },
-  { value: '3', label: 'Pear' },
+  { value: '0-apple', label: 'Apple' },
+  { value: '1-banana', label: 'Banana' },
+  { value: '2-orange', label: 'Orange' },
+  { value: '3-pear', label: 'Pear' },
 ]
