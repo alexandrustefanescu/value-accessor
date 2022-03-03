548289e76e752008eed6647befbb4a87f756cc36
Alexandru Stefanescu <alexandru@stefanescu@dekra.com>
2022-03-03 17:18:00 +0100

    removed unncessary @Input, changed the HTML structure in order to work correctly as a FormArray

--- src/app/reactive-form/form-components/dropdown/dropdown.component.ts
+++ src/app/reactive-form/form-components/dropdown/dropdown.component.ts
@@ -1,11 +1,12 @@
 import { Component, forwardRef, Input } from '@angular/core';
-import { ControlValueAccessor, FormGroup, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { AbstractControl, ControlValueAccessor, FormGroup, NG_VALUE_ACCESSOR, ValidationErrors } from '@angular/forms';
+import { of } from 'rxjs';
 import { FruitsData } from 'src/app/mock-data/fruits';
-
 @Component({
   selector: 'app-dropdown',
   templateUrl: './dropdown.component.html',
   styleUrls: ['./dropdown.component.scss'],
+
   // This part must always be provided in the component to be able to use the NG-VALUE-ACCESSOR
   providers: [{
     provide: NG_VALUE_ACCESSOR,
@@ -16,36 +17,47 @@ import { FruitsData } from 'src/app/mock-data/fruits';
 export class DropdownComponent implements ControlValueAccessor {
   // in Angular 12+ we MUST initialize a variable with value so we use ! when we just declare them
   @Input() parentForm!: FormGroup;
-  @Input() fieldName!: string;
   @Input() label!: string; 
 
   optionsList = FruitsData;
 
   value!: string;
-  changed!: (value: string) => void;
-  touched!: () => void;
+  private onChanged!: Function;
+  private onTouched!: Function;
   isDisabled!: boolean;
 
   constructor() { }
+
+  selectOption(option: string, id: string) {
+    this.value = option;
+    this.parentForm.patchValue(
+      { 
+        dropdown: [{ id, label: option }] 
+      },
+    ); 
+  }
 
   // called to write when programmatic changes from model to view are requested.
   writeValue(value: string): void {
     this.value = value;
   }
-  onChange(event: Event): void {
-    const value: string = (<HTMLInputElement>event.target).value;
-    this.changed(value);
-  }
   // is called when the control's value changes in the UI.
   registerOnChange(fn: any): void {
-    this.changed = fn;
+    this.onChanged = fn;
   }
   // is called to update the form model on blur.
   registerOnTouched(fn: any): void {
-    this.touched = fn;
+    this.onTouched = fn;
   }
   // when the control status changes to or from 'DISABLED'.
   setDisabledState(isDisabled: boolean): void {
     this.isDisabled = isDisabled;
   }
+}
+
+export function ValidateDropdown(control: AbstractControl): ValidationErrors | null {
+  const controlForm = control.value;
+  const result: Boolean = controlForm.some((item: { id: any; }) => item.id === null);
+  
+  return (result) ? { 'dropdownInvalid': true } : null;
 }
